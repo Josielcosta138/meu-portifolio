dd365bce1f76e8b7b0a3b5a83f094e19b28e694e
Josiel <cjosiel2@gmail.com>
2025-04-27 21:09:02 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
+// https://vite.dev/config/ teste
 export default defineConfig({
   plugins: [react()],
   base: '/meu-portifolio/',
